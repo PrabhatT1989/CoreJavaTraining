9848974324fb494223fb4a79bf3984c18da2fd74
PrabhatT1989 <prabhat.tavag@gmail.com>
2022-03-19 02:20:04 +0530

    Advance Method Overloading

--- src/AdvanceOverloading.java
+++ src/AdvanceOverloading.java
@@ -1,8 +1,21 @@
-
+class Aol
+{
+	public void Test(int a , String b)
+	{
+		System.out.println("calling int and long");
+	}
+	public void Test(long a , int b)
+	{
+		System.out.println("calling long and int");
+	}
+}
 public class AdvanceOverloading {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Aol a = new Aol();
+		a.Test(10, 20);
 
 	}
 
--- src/MethodOverloading.java
+++ src/MethodOverloading.java
@@ -1,8 +1,22 @@
-
+class Mol
+{
+	public void Test(int a,String b)
+	{
+		System.out.println("Calling int");
+	}
+	
+	public void Test()
+	{
+		System.out.println("calling String");
+	}
+}
 public class MethodOverloading {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Mol m = new Mol();
+		m.Test(10,"Test");
 
 	}
 
